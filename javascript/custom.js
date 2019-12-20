6756c688064e49cdc9557ce78d6c310308980f37
bakhtiyar <bakhtiyarkhozha@gmail.com>
2019-12-20 10:29:16 +0500

    update

--- javascript/custom.js
+++ javascript/custom.js
@@ -1,4 +1,4 @@
 $(document).ready(function () {
-    $('input[name="telephone"]').mask("+998(99) 999-99-99", {placeholder: "+998(__) ___-__-__ "});
+    $(".navbar-nav li").click(function(){ $(this).toggleClass("is-active")});
 
 });
